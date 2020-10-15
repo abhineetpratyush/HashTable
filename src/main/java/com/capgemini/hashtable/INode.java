cef8b4e93f405399e095e3a22d737cc8087d02b4
abhineetpratyush <abhineetpratyush1998@gmail.com>
2020-10-15 21:11:33 +0530

    [abhineet] add : ability to find frequency of words in a sentence like 'To be or not to be'

---
+++ src/main/java/com/capgemini/hashtable/INode.java
@@ -0,0 +1,8 @@
+package com.capgemini.hashtable;
+
+public interface INode<K> {
+	public K getKey();
+	public void setKey(K key);
+	public INode<K> getNext();
+	public void setNext(INode<K> next);
+}
